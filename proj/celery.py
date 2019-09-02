123b21946624e344a5b1f9bf4b591bc84e36892e
darkniter <Poranot@yandex.ru>
2019-09-02 12:49:37 +0300

    config with redis

--- proj/celery.py
+++ proj/celery.py
@@ -2,13 +2,14 @@ from __future__ import absolute_import, unicode_literals
 from celery import Celery
 
 app = Celery('proj',
-             broker='amqp://',
-             backend='amqp://',
+             broker='redis://localhost:6379/13',
+             backend='redis://localhost:6379/13',
              include=['proj.tasks'])
 
 # Optional configuration, see the application user guide.
 app.conf.update(
     result_expires=3600,
+
 )
 
 if __name__ == '__main__':
